a8b15dc88106e52973d6dceb56fbbbba1a129807
WojtekWietrzny <wietrzny@student.agh.edu.pl>
2024-05-07 00:18:02 +0200

    updated zip and deleted tempCodeRunner

--- MongoDB/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-average_rating = collection.aggregate([
-    {"$unwind": "$Trips"},
-    {"$unwind": "$Trips.attentants"},
-    {"$group": {"_id": None, "avgRating": {"$avg": "$Trips.attentants.review.rating"}}}
-])
-for result in average_rating:
-    print("\nAverage rating of all trips:", result["avgRating"])
-
-client.close()
